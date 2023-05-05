1ad6c8e3e1c1a9712e0a0c0d460ad139c0ebd875
duongduc1520 <132662742+duongduc1520@users.noreply.github.com>
2023-05-05 17:52:38 +0700

    initProject

---
+++ src/main/java/com/manage/product/ManageProductApplication.java
@@ -0,0 +1,13 @@
+package com.manage.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageProductApplication.class, args);
+    }
+
+}
